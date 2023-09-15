20cd22b39cb010f17bc686d7123d9eb4c96052b5
Arun03Kumar <0arunkumar14@gmail.com>
2023-09-15 21:33:05 +0530

    scroll

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import 'remixicon/fonts/remixicon.css'
+import React from "react";
+import "remixicon/fonts/remixicon.css";
 
 const Nav = () => {
-    const navigation = ["Platforms", "Partnerships", "Use cases", "About", "News", "Contact"]
+  const navigation = [
+    "Platforms",
+    "Partnerships",
+    "Use cases",
+    "About",
+    "News",
+    "Contact",
+  ];
   return (
     <nav>
-        <div className='leftNav'>
-            <div className='logo'>
-                <div className='innerC'></div>
-            </div>
-            <p>twinbru</p>
+      <div className="leftNav">
+        <div className="logo">
+          <div className="innerC"></div>
         </div>
-        <div className='centerNav'>
-            {navigation.map((item, i) => <p>{item}</p>)}
-        </div>
-        <div className='rightNav'>
+        <p>twinbru</p>
+      </div>
+      <div className="centerNav">
+        {navigation.map((item, i) => (
+          <p>{item}</p>
+        ))}
+      </div>
+      <div className="rightNav">
         <i class="ri-menu-4-line"></i>
-        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
